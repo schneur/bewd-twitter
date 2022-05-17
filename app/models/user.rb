a4c200cf0183ce01f2c8170a53b1f43c3ce38d60
root <root@LAPTOP-4JJ0VH4H.localdomain>
2022-05-17 18:44:18 -0400

    user model initial modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  
+  validates :username, presence: true, length: { minimum: 3, maximum: 64 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+  validates :email, presence: true, length: { minimum: 5, maximum: 500 }
+
+  validates_uniqueness_of :username
+  validates_uniqueness_of :password
+
+  has_many :sessions
+  has_many :tweets
+
 end
