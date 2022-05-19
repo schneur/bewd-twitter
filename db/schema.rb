2a419a123262f959c5f1096bffe396adfbdeb13e
root <root@LAPTOP-4JJ0VH4H.localdomain>
2022-05-19 19:23:50 -0400

    tweet table and model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_231907) do
+ActiveRecord::Schema.define(version: 2022_05_19_231523) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_05_17_231907) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
