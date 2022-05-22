da4f4ff948f0c27eb5d2900b354d333b73f59258
root <root@LAPTOP-4JJ0VH4H.localdomain>
2022-05-22 19:43:15 -0400

    user controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new{username: params[:username], email: params[:email], password: params[:password]}
 
+    if @user.save
+      render json: {
+        user: {
+          username: @user.username
+          email: @user.email
+        }
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :username)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/feeds'                    => 'feeds#index'
 
   # USERS
+  post '/users', to: 'users#create'
 
 
   # SESSIONS
