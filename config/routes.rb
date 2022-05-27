4099ddab3faa876f0342132286a80c46c58cf5a4
root <root@LAPTOP-4JJ0VH4H.localdomain>
2022-05-26 20:51:34 -0400

    sessions controller created

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   # USERS
   post '/users' => 'users#create'
 
-
   # SESSIONS
+  post '/sessions' => 'sessions#create'
 
+  get    '/authenticated'  =>  'sessions#authenticated'
+
+  delete '/sessions'  =>  'sessions#destroy'
 
   # TWEETS
-
+  post '/tweets' => 'tweets#create'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path'    => 'homepage#index'
